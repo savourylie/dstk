960ac9c17504f4908e97285c8dda755266a22b1b
Calvin Ku <calvin.j.ku@googlemail.com>
2018-05-07 11:37:25 +0800

    fixed test_imputer.py

--- tests/test_imputer.py
+++ tests/test_imputer.py
@@ -4,27 +4,26 @@ import pandas as pd
 import numpy as np
 
 
-def test_median_imputer():
+def test_categorical_imputer():
     df = pd.DataFrame(
         [[1, 2, 2, 8],
          [1, 1, np.nan, 3],
-         [np.nan, np.nan, np.nan],
+         [np.nan, np.nan, np.nan, np.nan],
          [2, 1, np.nan, -1]],
         columns=["a", "b", "c", "d"])
 
-    #df_result = df.fillna(df.mean())
     df_result=pd.DataFrame(
         [[1, 2, 2, 8],
          [1, 1, 2, 3],
-         [1, 1, 2, np.nan],
+         [1, 1, 2, -1],
          [2, 1, 2, -1]],
         columns=["a", "b", "c", "d"])
 
     imputer = Imputer(categorical_features=df.columns, categorical_method="most_freq_class")
     df = imputer.fit_transform(df)
-    print(df)
-    #assert df_result.equals(df)
+
+    assert np.isclose(df, df_result).all()
 
 
 if __name__ == '__main__':
-    test_median_imputer()
+    test_categorical_imputer()
